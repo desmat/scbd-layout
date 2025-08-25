69b6c0c55bb97c1f6983cb89ba22b99a9523776b
Mathieu Desjarlais <github@desmat.ca>
2025-08-25 16:42:27 -0400

    allow any icons in sidebar

--- components/layout/sidebar/nav-item-icon.vue
+++ components/layout/sidebar/nav-item-icon.vue
@@ -12,16 +12,7 @@
   lang="ts"
 >
 import { CIcon } from '@coreui/icons-vue';
-
-// NOTE: Only a subset of icons are included here as a demo
-// If wanting to adopt these icons (as opposed to font-awesome for kronos for example)
-// this would be the recommended approach: https://coreui.io/vue/docs/components/icon.html#icons-object
-import { cilList, cilCircle, cilHome, cilBuilding, cilContact, cilListRich, cilUser } from '@coreui/icons';
-
-// NOTE: this would be an alternative (but less ideal) approach
-// import * as icons from '@coreui/icons';
-
-const icons = { cilList, cilCircle, cilHome, cilBuilding, cilContact, cilListRich, cilUser };
+import * as icons from '@coreui/icons';
 
 const props = defineProps<{
   icon?: string,
